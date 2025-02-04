4bafdaa960d9940bfe219089bd8ecb52d96fdbcc
yuukilight <yuukilight@foxmail.com>
2025-02-04 23:15:40 +0800

    添加门电路部分内容

---
+++ ComputerOrgnization/verilog_code/gate_circut/and_gate.v
@@ -0,0 +1,8 @@
+module And(input a, b, output out);
+  Nand g1(a, b, AnandB);
+  Nand g2(AnandB, AnandB, out);
+endmodule
+module And(input a, b, output out);
+  Nand g1(a, b, AnandB);
+  Nand g2(AnandB, AnandB, out);
+endmodule
